b574a50636a46afd471f1bbf54496e3e419cd287
sweethua03 <91724689+sweethua03@users.noreply.github.com>
2022-10-08 16:39:37 +0800

    Add files via upload

---
+++ JAVA/JavaEE/Source/Practice/practice10/Circle.java
@@ -0,0 +1,23 @@
+package Source.Practice.practice10;
+
+public class Circle {
+
+    private double radius;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
+
+
