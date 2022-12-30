bf2c6f19bc97fc1b032ec420021c391c05216921
sweethua03 <1799198374@qq.com>
2022-12-30 13:02:48 +0800

    Nothing
    111

---
+++ CPP/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    cout << "This is a test" << endl;
+
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "This is another test." << endl;
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
