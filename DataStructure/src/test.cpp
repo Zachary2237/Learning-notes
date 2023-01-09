43bff716aa89972f499157bbe7bc524fb482beca
sweethua03 <1799198374@qq.com>
2023-01-09 17:23:10 +0800

    1

--- DataStructure/src/test.cpp
+++ DataStructure/src/test.cpp
@@ -3,11 +3,40 @@
 //
 
 #include "iostream"
+#include "string"
 
 using namespace std;
 
+struct Student {
+    int id;
+    int score;
+};
+
+struct Person {
+    string name;
+    int age;
+    struct Student s;
+};
+
+void printStruct(Person *p) {
+    p->age = 29;
+    p->s.score = 64;
+    cout << p->name << " " << p->age << " " << p->s.id << " " << p->s.score << endl;
+}
+
 int main() {
-    cout << "This is a test!" << endl;
+
+    Person p;
+    p.name = "Tom";
+    p.age = 19;
+    p.s.id = 1;
+    p.s.score = 98;
+
+    cout << p.name << " " << p.age << " " << p.s.id << " " << p.s.score << endl;
+
+    printStruct(&p);
+
+
 
     return 0;
 }
