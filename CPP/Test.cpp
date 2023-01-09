3b06b08e7406a8c92920aa4b86e6c14409736b4b
sweethua03 <1799198374@qq.com>
2023-01-09 17:24:20 +0800

    1

--- CPP/Test.cpp
+++ CPP/Test.cpp
@@ -1,13 +1,11 @@
-//
-// Created by iimmy_o on 2022/12/28.
-//
-
-#include "iostream"
+#include<iostream>
 
 using namespace std;
 
 int main() {
-    cout << "This is a test!" << endl;
+
+    cout << "This is a test." << endl;
+    cout << "Hello World!" << endl;
 
     return 0;
 }
--- JavaEE/src/Chapter1_to_7/Practice/practice10/CompareObject.java
+++
@@ -1,5 +0,0 @@
-package Chapter1_to_7.Practice.practice10;
-
-public interface CompareObject {
-    public int compareTo(Object o);
-}
